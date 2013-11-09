101ebbfce791bf0f169869e77c15376172b9d296
ejhunter <ejhunter@cablecity-mintx>
2013-11-09 14:11:02 +0000

    Implemented mouse lead resizing, mouse sectoring and mouse wheel resizing.

--- uk/ejhunter/tintedwindow/update/Window.java
+++ uk/ejhunter/tintedwindow/update/Window.java
@@ -5,6 +5,7 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 import javax.swing.JFrame;
 
@@ -30,6 +31,9 @@ public class Window extends JFrame {
 
     private class Dragger extends MouseAdapter {
 
+        private static final int BORDER_SIZE = 20;
+        private static final int SCROLL_FACTOR = 5;
+
         private int startX;
         private int startY;
 
@@ -44,41 +48,162 @@ public class Window extends JFrame {
             this.startY = e.getY();
 
             this.dragX = e.getXOnScreen();
-            this.dragY = e.getXOnScreen();
+            this.dragY = e.getYOnScreen();
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
             if (e.isShiftDown()) {
-                setSize(getWidth() + (e.getXOnScreen() - dragX), getHeight());
+                switch (mouseSector) {
+                    case NW:
+                        setSize(getWidth(), getHeight() - (e.getYOnScreen() - dragY));
+                        setLocation(getX(), getY() + (e.getYOnScreen() - dragY));
+
+                        setSize(getWidth() - (e.getXOnScreen() - dragX), getHeight());
+                        setLocation(getX() + (e.getXOnScreen() - dragX), getY());
+                        break;
+                    case N:
+                        setSize(getWidth(), getHeight() - (e.getYOnScreen() - dragY));
+                        setLocation(getX(), getY() + (e.getYOnScreen() - dragY));
+                        break;
+                    case NE:
+                        setSize(getWidth(), getHeight() - (e.getYOnScreen() - dragY));
+                        setLocation(getX(), getY() + (e.getYOnScreen() - dragY));
+
+                        setSize(getWidth() + (e.getXOnScreen() - dragX), getHeight());
+                        break;
+                    case E:
+                        setSize(getWidth() + (e.getXOnScreen() - dragX), getHeight());
+                        break;
+                    case SE:
+                        setSize(getWidth(), getHeight() + (e.getYOnScreen() - dragY));
+
+                        setSize(getWidth() + (e.getXOnScreen() - dragX), getHeight());
+                        break;
+                    case S:
+                        setSize(getWidth(), getHeight() + (e.getYOnScreen() - dragY));
+                        break;
+                    case SW:
+                        setSize(getWidth(), getHeight() + (e.getYOnScreen() - dragY));
+
+                        setSize(getWidth() - (e.getXOnScreen() - dragX), getHeight());
+                        setLocation(getX() + (e.getXOnScreen() - dragX), getY());
+                        break;
+                    case W:
+                        setSize(getWidth() - (e.getXOnScreen() - dragX), getHeight());
+                        setLocation(getX() + (e.getXOnScreen() - dragX), getY());
+                        break;
+                    case NONE:
+                    default:
+                        break;
+                }
 
                 this.dragX = e.getXOnScreen();
-                this.dragY = e.getXOnScreen();
+                this.dragY = e.getYOnScreen();
             } else if (e.isControlDown()) {
                 setLocation(e.getXOnScreen() - this.startX, e.getYOnScreen() - this.startY);
             }
         }
 
         @Override
-        public void mouseClicked(MouseEvent e) {
-            // System.out.println("Clicked");
-        }
-
-        @Override
         public void mouseMoved(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+            int h = getHeight();
+            int w = getWidth();
+
+            if (x < BORDER_SIZE && y < BORDER_SIZE)
+                mouseSector = Sector.NW;
+            else if (x > BORDER_SIZE && x < (w - BORDER_SIZE) && y < BORDER_SIZE)
+                mouseSector = Sector.N;
+            else if (x > (w - BORDER_SIZE) && y < BORDER_SIZE)
+                mouseSector = Sector.NE;
+            else if (x > (w - BORDER_SIZE) && y > BORDER_SIZE && y < (h - BORDER_SIZE))
+                mouseSector = Sector.E;
+            else if (x > (w - BORDER_SIZE) && y > (h - BORDER_SIZE))
+                mouseSector = Sector.SE;
+            else if (x > BORDER_SIZE && x < (w - BORDER_SIZE) && y > (h - BORDER_SIZE))
+                mouseSector = Sector.S;
+            else if (x < BORDER_SIZE && y > (h - BORDER_SIZE))
+                mouseSector = Sector.SW;
+            else if (x < BORDER_SIZE && y > BORDER_SIZE && y < (h - BORDER_SIZE))
+                mouseSector = Sector.W;
+            else
+                mouseSector = Sector.NONE;
+
             if (e.isShiftDown()) {
-                int x = e.getX();
-                int y = e.getY();
-                int h = getHeight();
-                int w = getWidth();
-
-                // if() //TODO: mouse sector choosing
-
-                setCursor(Cursor.getPredefinedCursor(Cursor.N_RESIZE_CURSOR));
+                switch (mouseSector) {
+                    case NW:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.NW_RESIZE_CURSOR));
+                        break;
+                    case N:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.N_RESIZE_CURSOR));
+                        break;
+                    case NE:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.NE_RESIZE_CURSOR));
+                        break;
+                    case E:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR));
+                        break;
+                    case SE:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.SE_RESIZE_CURSOR));
+                        break;
+                    case S:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.S_RESIZE_CURSOR));
+                        break;
+                    case SW:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.SW_RESIZE_CURSOR));
+                        break;
+                    case W:
+                        setCursor(Cursor.getPredefinedCursor(Cursor.W_RESIZE_CURSOR));
+                        break;
+                    case NONE:
+                    default:
+                        setCursor(Cursor.getDefaultCursor());
+                        break;
+                }
             } else if (e.isControlDown()) {
                 setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
             } else {
                 setCursor(Cursor.getDefaultCursor());
+            }
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            if (e.isShiftDown()) {
+                switch (mouseSector) {
+                    case NW:
+                        setLocation(e.getXOnScreen(), e.getYOnScreen());
+                        break;
+                    case N:
+                        break;
+                    case NE:
+                        break;
+                    case E:
+                        break;
+                    case SE:
+                        break;
+                    case S:
+                        break;
+                    case SW:
+                        break;
+                    case W:
+                        break;
+                    case NONE:
+                    default:
+                        break;
+                }
+            } else {
+                mouseSector = Sector.NONE;
+            }
+        }
+
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            if (e.isShiftDown()) {
+                setSize(getWidth() - (e.getWheelRotation() * SCROLL_FACTOR), getHeight() - (e.getWheelRotation() * SCROLL_FACTOR));
+                setLocation(getX() + (int) (e.getWheelRotation() * SCROLL_FACTOR * 0.5f), getY() + (int) (e.getWheelRotation() * SCROLL_FACTOR * 0.5f));
             }
         }
     }
